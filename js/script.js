d68c54c06d8652fd0d734bd0f2d13d661b58a0aa
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-11-26 01:27:52 +0100

    terzo commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
 
+    $('.p_relative').mouseenter(
+      function() {
+        $(this).children('div').first().removeClass('display_none');
+        console.log(this);
+    });
+    $('.p_relative').mouseleave(
+      function() {
+        $(this).children('div').addClass('display_none');
+    })
+  }
+);
